841f273f28368ff7fe978a63675d508ea1534991
Joe Mruz <mruz.joe@gmail.com>
2026-08-24 01:20:03 -0400

    Make item level rebalance more subtle

--- crawl-ref/source/dungeon.cc
+++ crawl-ref/source/dungeon.cc
@@ -4355,12 +4355,11 @@ static void _builder_items()
         specif_type = OBJ_GOLD;  // Lots of gold in the orcish mines.
         items_levels *= 2;       // Four levels' worth, in fact.
     }
-    // JM: Keep items rates the same but much more powerful. This avoids an overload of items,
-    // but makes those finds more meaningful.
+    // JM: Increase the quality of items
     else
     {
         //items_wanted /= 2;
-        items_levels *= 20;
+        items_levels *= 5;
     }
 
     for (i = 0; i < items_wanted; i++)
